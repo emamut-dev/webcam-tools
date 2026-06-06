ab5df545dda8bb598db4880ca1a41bb7eed0d0da
Faber Vergara <fabervergara@gmail.com>
2026-06-06 18:44:50 -0500

    fix: move nav menu to component

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,9 @@
-<script setup></script>
+<script setup>
+import Menu from '@/components/menu.vue';
+</script>
 
 <template>
-  <nav
-    class="navbar navbar-expand-lg bg-dark border-bottom border-body"
-    data-bs-theme="dark"
-  >
-    <div class="container-fluid">
-      <RouterLink class="navbar-brand" to="/">Webcam Tools</RouterLink>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <RouterLink to="/" class="nav-link active" aria-current="page"
-              >Inicio</RouterLink
-            >
-          </li>
-          <li class="nav-item">
-            <RouterLink to="/tokens-calculator" class="nav-link"
-              >Calculadora de Tokens</RouterLink
-            >
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
+  <Menu />
   <div class="container">
     <router-view />
   </div>
---
+++ src/components/menu.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+<template>
+  <nav
+    class="navbar navbar-expand-lg nav-underline bg-dark border-bottom border-body"
+    data-bs-theme="dark"
+  >
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">Webcam Tools</RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <RouterLink
+              to="/"
+              class="nav-link"
+              active-class="active"
+              exact-active-class="active"
+              >Inicio</RouterLink
+            >
+          </li>
+          <li class="nav-item">
+            <RouterLink
+              to="/tokens-calculator"
+              class="nav-link"
+              active-class="active"
+              >Calculadora de Tokens</RouterLink
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
