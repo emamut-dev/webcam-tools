8f37f15cd553866daf03c8d5bcb33d391d87e598
Faber Vergara <fabervergara@gmail.com>
2026-06-06 16:45:22 -0500

    feat: add tokens calculator

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
-<script setup>
-import TokensCalculator from './components/TokensCalculator.vue';
-</script>
+<script setup></script>
 
 <template>
-  <TokensCalculator />
+  <nav
+    class="navbar navbar-expand-lg bg-dark border-bottom border-body"
+    data-bs-theme="dark"
+  >
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">Webcam Tools</RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <RouterLink
+              to="/tokens-calculator"
+              class="nav-link active"
+              aria-current="page"
+              >Tokens Calculator</RouterLink
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <router-view />
+  </div>
 </template>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <div class="row mt-3">
+      <div class="col text-center">
+        <h1>Home Page</h1>
+      </div>
+    </div>
+  </div>
+</template>
