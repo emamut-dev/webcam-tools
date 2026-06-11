1f56cbb92afc8b61b648349ddb3584d74504ea47
Faber Vergara <fabervergara@gmail.com>
2026-06-11 10:33:20 -0500

    fix: get fonts from fontsource

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import { createApp } from 'vue';
 import router from './router/';
 
-import './style.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/600.css';
+import '@fontsource/montserrat/700.css';
 
 import App from './App.vue';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import './style.css';
 
 createApp(App).use(router).mount('#app');
