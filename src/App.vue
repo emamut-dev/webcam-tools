2a8f176c90933125eab09a8921af6b7f4ec6c312
Faber Vergara <fabervergara@gmail.com>
2026-06-12 15:13:03 -0500

    fix: move footer to component and fix layout

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <script setup>
 import Menu from '@/components/menu.vue';
+import Footer from '@/components/footer.vue';
 </script>
 
 <template>
   <Menu />
-  <div class="container min-vh-100">
+  <!-- <div class="container min-vh-100"> -->
+  <div class="container">
     <router-view />
   </div>
-  <footer class="row">
-    <div class="col text-center">
-      <p class="text-white-50 mt-5 mb-3">
-        <i class="bi bi-rocket-takeoff text-warning"></i> Powered by
-        <a
-          class="text-warning"
-          href="https://emamut.netlify.app/"
-          target="_blank"
-          >emamut</a
-        >
-      </p>
-    </div>
-  </footer>
+  <Footer />
 </template>
---
+++ src/components/footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="container">
+    <div class="row justify-content-between">
+      <div class="col">
+        <p class="text-white-50 mt-5 mb-3">
+          Derechos reservados <i class="bi bi-c-circle text-warning"></i> 2026
+          Webcam Tools.
+        </p>
+      </div>
+      <div class="col text-end">
+        <p class="text-white-50 mt-5 mb-3">
+          Powered by
+          <a
+            class="text-warning"
+            href="https://emamut.netlify.app/"
+            target="_blank"
+            ><img
+              src="../assets/logo-emamut-mini.png"
+              id="logo-emamut"
+              alt="emamut"
+              class="img-fluid"
+          /></a>
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
